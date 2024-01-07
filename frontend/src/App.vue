3408f8d5e7839fba88234b604e43ef74c64947b8
dajeong <dajung5623@naver.com>
2024-01-07 14:41:28 +0900

    게시판 프로젝트

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- 글쓰기 버튼 -->
+    <el-button type="danger" @click="openRegistPopup()">글쓰기</el-button>
+    <regist-board @reload="reload" ref="registPopup"></regist-board>
+    <board-list ref="list"></board-list>
+  </div>
+</template>
+
+<script>
+import RegistBoard from './components/RegistBoard.vue';
+import BoardList from './components/BoardList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RegistBoard,
+    BoardList,
+  },
+  methods: {
+    reload() {
+      this.$refs.list.getBoardList();
+    },
+    openRegistPopup() {
+      this.$refs.registPopup.openPopup = true;
+    }
+  },
+};
+</script>
+
+<style></style>
